00afa4359ce8c9821c790e32568cf64979e887ed
Khairul <Khairulimran6810@gmail.com>
2023-10-18 21:22:13 +0800

    Added some syntax

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to your shopping cart");
+
+        Console cons = System.console();
+
+        String name = cons.readLine("What is your name?");
+
+        System.out.printf("Hello %s. Pleased to meet you.\n", name);
     }
 }
